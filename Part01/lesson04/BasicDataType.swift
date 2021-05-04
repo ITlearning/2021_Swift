0585d1f6df1accbcf7dd254e80019e236b23eb85
ITlearning <yo7504@naver.com>
2021-05-04 18:37:21 +0900

    05월 04일 최종 커밋

---
+++ Part01/lesson04/BasicDataType.swift
@@ -0,0 +1,42 @@
+/* 기본 데이터 타입 */
+//import UIKit
+import Swift
+// Swift의 기본 데이터 타입
+// Bool, Int, UInt, Float, Double, Character, String
+
+// Bool
+var someBool: Bool = true
+someBool = false
+print(someBool)
+
+//Int
+var someInt: Int = -100
+//someInt = 100.1
+
+//UInt
+var someUInt: UInt = 100
+//someUInt = -100
+//someUInt = someInt
+
+// Float(32bit)
+var someFloat: Float = 3.14
+someFloat = 3
+
+//Double(64bit)
+var someDouble: Double = 3.14
+someDouble = 3
+//someDouble = someFloat
+
+//Character
+var someCharacter: Character = "?"
+//someCharacter = "^"
+//someCharacter = "가"
+//someCharacter = "A"
+//someCharacter = "하하하"
+print(someCharacter)
+
+//String
+var someString: String = "하하하"
+someString = someString + "안녕하세요"
+print(someString)
+//someString = someCharacter
